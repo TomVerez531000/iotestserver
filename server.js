d9726e3287fc31ee9a840cbee8819be4c49ef77d
TomVerez531000 <86794107+TomVerez531000@users.noreply.github.com>
2026-05-02 19:18:40 +0200

    Added player join handle

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const { v4: uuidv4 } = require('uuid');
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
 
+function send_data(ws, data) {
+  const text = JSON.stringify(data);
+  ws.send(text);
+}
+
+function player_join(ws, data) {
+  const nickname = data.nickname
+  if (nickname == null || nickname == "" || nickname.length > 20) {
+    var message = {};
+    message.type = "join-response";
+    message.success = false;
+    send_data(message);
+  }
+
+  var message = {};
+  message.type = "join-response";
+  message.success = true;
+  send_data(message);
+}
+
 wss.on("connection", (ws, req) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   ws.id = uuidv4();
   console.log(`Client connecté - ID: ${ws.id} - IP: ${ip}`);
   
   ws.on("message", (msg) => {
-    ws.send("Reçu: " + msg);
+    const data = JSON.parse(msg)
+    if (data.type == "join-game") {
+      player_join(ws, data);
+    }
   });
 });
