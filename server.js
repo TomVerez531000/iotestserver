483933a5258d4263591281db75819e6169d5bda5
TomVerez531000 <86794107+TomVerez531000@users.noreply.github.com>
2026-05-02 17:44:43 +0200

    Update server.js
    Added ip lookup and uuid identifier for a connection and a printer when a connection open

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
 
 wss.on("connection", (ws) => {
-  console.log("Client connecté");
-
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  ws.id = uuidv4();
+  console.log(`Client connecté - ID: ${ws.id} - IP: ${ip}`);
+  
   ws.on("message", (msg) => {
-    console.log("Message:", msg);
     ws.send("Reçu: " + msg);
   });
 });
