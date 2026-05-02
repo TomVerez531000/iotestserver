25e18061596e6424d02b60e0d92adfbd28b0d231
TomVerez531000 <86794107+TomVerez531000@users.noreply.github.com>
2026-05-02 17:50:52 +0200

    Fixed req

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, req) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   ws.id = uuidv4();
   console.log(`Client connecté - ID: ${ws.id} - IP: ${ip}`);
