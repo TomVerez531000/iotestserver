55981999318c6794b96742a174f27ac7388935ac
TomVerez531000 <86794107+TomVerez531000@users.noreply.github.com>
2026-05-03 18:45:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,51 @@ function send_data(ws, data) {
   ws.send(text);
 }
 
+const TICK_RATE = 20;
 players = {}
+setInterval(() => {
+  const dt = 1 / TICK_RATE;
+
+  for (let ws in players) 
+    if (ws.direction && (ws.direction.x !== 0 || ws.direction.y !== 0)) {
+      
+      if (ws.direction.x > 1 || ws.direction.y > 1) {ws.close();} // direction isnt normalized meaning the player try to speedhack with direction
+      ws.x += ws.direction.x * PLAYER_SPEED * dt;
+      ws.y += ws.direction.y * PLAYER_SPEED * dt;
+    }
+  }
+
+  broadcastPositions();
+}, 1000 / TICK_RATE);
+
+function broadcastPositions() {
+  const payload = {
+    type: "update_world",
+    players: {}
+  };
+
+  // On prépare les données (on n'envoie que le nécessaire)
+  for (let ws in players) {
+    payload.players[ws.id] = {
+      x: ws.x,
+      y: ws.y,
+      size: ws.size
+    };
+  }
+
+  const message = JSON.stringify(payload);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+function get_spawn_pos(ws) {
+  return {x:0, y:0}
+}
+
+var base_size = 100;
 function player_join(ws, data) {
   const nickname = data.nickname
   if (nickname == null || nickname == "" || nickname.length > 20) {
@@ -19,7 +63,12 @@ function player_join(ws, data) {
     return
   }
 
-  players[ws.id] = {};
+  players[ws] = true;
+  var pos = get_spawn_pos(ws);
+  ws.x = pos.x;
+  ws.y = pos.y;
+  ws.size = base_size;
+  
   var message = {};
   message.type = "join-response";
   message.success = true;
@@ -27,9 +76,8 @@ function player_join(ws, data) {
 }
 
 function update_direction(ws, data) {
-  if (players[ws.id] == null) {return}
-  players[ws.id].direction = data.direction
-  console.log(data.direction);
+  if (!players[ws]) {return}
+  ws.direction = data.direction;
 }
 
 wss.on("connection", (ws, req) => {
